a5133e416d941544567060370f83641bc0bc62b3
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-04-19 22:54:12 +0330

    add project fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import vazirFont from "@/constants/localFont";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased ${vazirFont.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   );
 }
