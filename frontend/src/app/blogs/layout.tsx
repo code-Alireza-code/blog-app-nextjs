f6db7c0b0157caa7f92aab4130ceae48ab3b7e4d
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-04-22 11:27:11 +0330

    add blog page & layout

---
+++ frontend/src/app/blogs/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "بلاگ ها",
+};
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-600 space-y-4">
+          category list
+        </div>
+        <div className="col-span-12 lg:col-span-8 xl:col-span-9 text-secondary-600 space-y-4">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
