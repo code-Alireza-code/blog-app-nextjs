f165c50064e07f663fa2c9a105c513accdf6926f
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-04-23 12:53:55 +0330

    Revert "add Suspense to CategoryList component"
    This reverts commit db0388dda9e9106fff45e6bb7e920e556692626d.

--- frontend/src/app/blogs/layout.tsx
+++ frontend/src/app/blogs/layout.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
-import { ReactNode, Suspense } from "react";
+import { ReactNode } from "react";
 import CategoryList from "./_components/CategoryList";
-import Spinner from "@/ui/Spinner";
 
 export const metadata: Metadata = {
   title: "بلاگ ها",
@@ -13,9 +12,7 @@ function Layout({ children }: { children: ReactNode }) {
       <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-600 space-y-4">
-          <Suspense fallback={<Spinner />}>
-            <CategoryList />
-          </Suspense>
+          <CategoryList />
         </div>
         <div className="col-span-12 lg:col-span-8 xl:col-span-9 text-secondary-600 space-y-4">
           {children}
