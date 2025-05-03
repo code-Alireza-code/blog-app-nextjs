e94f7916797ec2648c3bc926b12eaf90b5177195
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-05-03 11:18:50 +0330

    handle 401 error with axios

--- frontend/src/services/httpService.ts
+++ frontend/src/services/httpService.ts
@@ -1,9 +1,35 @@
-import axios from "axios";
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
 
 const app = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+
+app.interceptors.request.use(
+  (res) => res,
+  async (err: AxiosError) => {
+    const originalConfig = err.config as CustomAxiosRequestConfig;
+
+    if (err.response?.status === 401 && !originalConfig._retry) {
+      originalConfig._retry = true;
+      try {
+        const { data } = await app.get("/user/refresh-token");
+        if (data) return app(originalConfig as AxiosRequestConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
 const http = {
   get: app.get,
