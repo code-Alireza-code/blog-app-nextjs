9cb838537b3963c55a782cb1d07764a2afaeab27
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-05-03 12:20:18 +0330

    add middleware file to protect routes

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,27 @@
+import {
+  NextResponse,
+  type MiddlewareConfig,
+  type NextRequest,
+} from "next/server";
+import middlewareAuth from "./utils/middlewareAuth";
+
+export async function middleware(request: NextRequest) {
+  console.log(request);
+  if (request.nextUrl.pathname.startsWith("/profile")) {
+    const user = await middlewareAuth(request);
+    if (!user)
+      return NextResponse.redirect(new URL("/signin", request.nextUrl));
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith("/signin") ||
+    request.nextUrl.pathname.startsWith("/signup")
+  ) {
+    const user = await middlewareAuth(request);
+    if (user) return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+}
+
+export const config: MiddlewareConfig = {
+  matcher: ["/profile/:path*", "/signin", "/signup"],
+};
