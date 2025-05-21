103d205804a38c0cbf83b1650bc20219e5d15e29
Alireza mohammadi <alireza.mohammadi.m19@gmail.com>
2025-05-21 12:14:37 +0330

    add search feature to profile/posts

--- frontend/src/app/(dashboard)/profile/posts/_/components/Buttons.tsx
+++ frontend/src/app/(dashboard)/profile/posts/_/components/Buttons.tsx
@@ -2,6 +2,7 @@
 
 import ButtonIcon from "@/ui/ButtonIcon";
 import Link from "next/link";
+import { FaPlus } from "react-icons/fa6";
 import { IoMdTrash } from "react-icons/io";
 import { MdEdit } from "react-icons/md";
 
@@ -29,6 +30,18 @@ export function EditPost({ postId }: Props) {
       <ButtonIcon onClick={handleEdit} variant="outline">
         <MdEdit />
       </ButtonIcon>
+    </Link>
+  );
+}
+
+export function CreatePost() {
+  return (
+    <Link
+      href="/profile/posts/create"
+      className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium transition-colors hover:bg-primary-700 duration-300 ease-out"
+    >
+      <span className="hidden md:block">ایجاد پست</span>
+      <FaPlus className="size-5" />
     </Link>
   );
 }
